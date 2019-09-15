4df6dbade9b22d16cd0e0da3776ba847194fc098
Murray Le Roux <murray@avantgardeweb.co.uk>
2019-09-15 18:06:04 +0100

    Added webpack

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+module.exports = {
+    entry: './index.js',
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true,
+                            modules: true,
+                            importLoaders: 1
+                        }
+                    },
+                    {
+                        loader: 'sass-loader?outputStyle=expanded',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true,
+                            importLoaders: 1
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
+    }
+};
